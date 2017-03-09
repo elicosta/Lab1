7be10aa84b1b7245366b30390762b3594c436227
Elivelton Costa <elivelton_costa@live.com>
2017-03-09 13:33:32 -0300

    Create main.cpp
    Implementação da função principal do programa

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Calculadora de Geometria Plana e Espacial" << endl;
+	cout << "(1) Triangulo equilatero" << endl;
+	cout << "(2) Retangulo" << endl;
+	cout << "(3) Quadrado" << endl;
+	cout << "(4) Circulo" << endl;
+	cout << "(5) Piramide com base quadrangular" << endl;
+	cout << "(6) Cubo" << endl;
+	cout << "(7) Paralelepipedo" << endl;
+	cout << "(8) Esfera" << endl;
+	cout << "(0) Sair" << endl;
+
+	return 0;
+}
