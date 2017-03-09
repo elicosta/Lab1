206ea5c4f0650d3c42d77a49547fa8ee22354054
Elivelton Costa <elivelton_costa@live.com>
2017-03-09 13:58:01 -0300

    Update main.cpp
    Implementação do painel de opções

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main(){
+	
+	int op, alt, larg, comp, raio;
 
 	cout << "Calculadora de Geometria Plana e Espacial" << endl;
 	cout << "(1) Triangulo equilatero" << endl;
@@ -14,6 +16,58 @@ int main(){
 	cout << "(7) Paralelepipedo" << endl;
 	cout << "(8) Esfera" << endl;
 	cout << "(0) Sair" << endl;
+	
+	switch (op){
+        	case 1:
+            		cout << "Qual os valores do Triangulo equilatero?" << endl;
+			cout << "Altura: ";
+			cin >> alt;
+			cout << "Largura: ";
+			cin >> larg;
+            		break;
+        	case 2:
+            		cout << "Qual os valores do Retangulo?" << endl;
+			cout << "Altura: ";
+			cin >> alt;
+			cout << "Largura: ";
+			cin >> larg;
+            		break;
+        	case 3:
+            		cout << "Qual os valores do Quadradro?" << endl;
+			cout << "Altura: ";
+			cin >> alt;
+            		break;
+		case 4:
+			cout << "Qual os valores do Circulo?" << endl;
+			cout << "Raio: ";
+			cin >> raio;
+            		break;
+		case 5:
+			cout << "Qual os valores do Piramide com base quadrangular?" << endl;
+            		break;
+		case 6:
+			cout << "Qual os valores do Cubo?" << endl;
+			cout << "Aresta: "; //Aresta usando variavel de altura
+			cin >> alt;
+            		break;
+		case 7:
+			cout << "Qual os valores do Paralelepipedo?" << endl;
+			cout << "Altura: ";
+			cin >> alt;
+			cout << "Largura: ";
+			cin >> larg;
+			cout << "Comprimento: ";
+			cin >> comp;
+            		break;
+		case 8:
+			cout << "Qual os valores da Esfera?" << endl;
+			cout << "Raio: ";
+			cin >> raio;
+            		break;
+		default:
+			cout << "Opção inválida!" << endl;
+			break;
+    	}
 
 	return 0;
 }
